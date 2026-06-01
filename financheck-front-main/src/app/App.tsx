247a5eb2e9a347945bec4d8b836c5ee2c52c3b5a
adri-oliver <144168326+adri-oliver@users.noreply.github.com>
2026-05-31 22:03:07 -0300

    Update App.tsx

--- financheck-front-main/src/app/App.tsx
+++ financheck-front-main/src/app/App.tsx
@@ -1,11 +1,13 @@
 import { RouterProvider } from 'react-router';
 import { AuthProvider } from './context/AuthContext';
 import { router } from './routes';
+import AccessibilityButton from "./components/AccessibilityButton";
 
 export default function App() {
   return (
     <AuthProvider>
       <RouterProvider router={router} />
+      <AccessibilityButton />
     </AuthProvider>
   );
 }
