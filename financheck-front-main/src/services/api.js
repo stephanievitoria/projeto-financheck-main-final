a8cb79f40ee6888a3531dc696ee8003a03c442e3
Lns14 <153565972+Lns14@users.noreply.github.com>
2026-05-31 21:31:06 -0300

    Update api.js

--- financheck-front-main/src/services/api.js
+++ financheck-front-main/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8082"
+  baseURL: "https://projeto-finacheck-main-back.onrender.com"
 });
 
 export default api;
